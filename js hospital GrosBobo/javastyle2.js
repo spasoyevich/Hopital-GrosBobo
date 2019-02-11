dbb447081ea66ae3c98c740f04f6a20476c6cd0c
spasoyevich <46974050+spasoyevich@users.noreply.github.com>
2019-02-11 09:16:32 +0100

    Add files via upload

---
+++ js hospital GrosBobo/javastyle2.js
@@ -0,0 +1,76 @@
+function epaule() {
+  var heading = document.createElement("H4");
+  var txt1 = document.createTextNode("Epaule");
+  heading.appendChild(txt1);
+  document.getElementById("epaule").appendChild(heading);
+
+  var para = document.createElement("P");
+  var txt2 = document.createTextNode("Elle comporte plusieurs articulations qui concourent à en faire le complexe articulaire le plus mobile du corps humain. Elle permet d'orienter le membre supérieur dans l'espace, autorisant en particulier son extrémité effectrice, la main, à assurer ses rôles de préhension et de communication avec l'environnement situé à sa portée. ");
+  para.appendChild(txt2);
+  document.getElementById("epaule").appendChild(para);
+
+}
+function main(){
+  var heading = document.createElement("H4");
+  var txt1 = document.createTextNode("Main");
+  heading.appendChild(txt1);
+  document.getElementById("main").appendChild(heading);
+
+  var para = document.createElement("P");
+  var txt2 = document.createTextNode("La main (du latin : manus, « côté du corps ») est l’organe préhensile effecteur situé à l’extrémité de l’avant-bras et relié à ce dernier par le poignet. C'est un organe capable notamment de saisir et manipuler des objets. Chez l'homme, la main est un organe extrêmement développé et important, elle dispose d'une palette d'actions très large. Située à l'extrémité des deux membres supérieurs, chaque main possède cinq doigts qui apportent une contribution majeure au sens du toucher.");
+  para.appendChild(txt2);
+  document.getElementById("main").appendChild(para);
+
+}
+
+function genous(){
+  var heading = document.createElement("H4");
+  var txt1 = document.createTextNode("Genous");
+  heading.appendChild(txt1);
+  document.getElementById("genous").appendChild(heading);
+
+  var para = document.createElement("P");
+  var txt2 = document.createTextNode("Le genou est une articulation qui permet de joindre la jambe à la cuisse. Elle met en jeu trois os, le fémur, le tibia et la patella, par le biais de trois articulations, l'articulation fémoro-patellaire et la double articulation fémoro-tibiale. Le cartilage assure la fluidité des mouvements du genou. Le tissu élastique fin, le cartilage, protège l'os et fait en sorte que les surfaces de l'articulation glissent facilement les unes contre les autres");
+  para.appendChild(txt2);
+  document.getElementById("genous").appendChild(para);
+
+}
+
+function coude(){
+  var heading = document.createElement("H4");
+  var txt1 = document.createTextNode("Coude");
+  heading.appendChild(txt1);
+  document.getElementById("coude").appendChild(heading);
+
+  var para = document.createElement("P");
+  var txt2 = document.createTextNode("Le coude (ou articulation olécranienne) est la partie du membre supérieur située entre le bras et l'avant-bras. Cette articulation comprend en avant la région du « pli du coude » (ou dedans de coude). C'est un complexe articulaire synovial du membre supérieur humain reliant le bras à l'avant-bras. Il unit ainsi trois os entre eux : le radius, l'ulna (cubitus) et l'humérus.");
+  para.appendChild(txt2);
+  document.getElementById("coude").appendChild(para);
+
+}
+
+function cheville(){
+  var heading = document.createElement("H4");
+  var txt1 = document.createTextNode("Cheville");
+  heading.appendChild(txt1);
+  document.getElementById("cheville").appendChild(heading);
+
+  var para = document.createElement("P");
+  var txt2 = document.createTextNode("La cheville ou cou-de-pied est l'articulation qui relie la jambe et le pied.Elle est parfois sujette à des entorses, le plus souvent externes par flexion plantaire et pied en équin.Une cheville adulte est composée, au point de vue osseux, de l'épiphyse inférieure du tibia (malléole interne et plafond), de l\'épiphyse inférieure de la fibula (ou péroné) (malléole externe) et du talus (ou astragale). Classiquement, on parle d\'articulation supinale de l\'arrière pied. ");
+  para.appendChild(txt2);
+  document.getElementById("cheville").appendChild(para);
+
+}
+
+
+function hanche(){
+  var heading = document.createElement("H4", "width=200,height=200");
+  var txt1 = document.createTextNode("Hanche");
+  heading.appendChild(txt1);
+  document.getElementById("hanche").appendChild(heading);
+
+  var para = document.createElement("P",'width=100,height=100');
+  var txt2 = document.createTextNode("La hanche ou articulation coxo-fémorale est une articulation (énarthrose) qui permet de joindre la cuisse au bassin. Elle met en jeu deux os : l'os iliaque et le fémur. ");
+  para.appendChild(txt2);
+  document.getElementById("hanche").appendChild(para);
+}
